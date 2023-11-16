7e450eaef375efa06497b6cab1da261fd467b93f
Corro-Kun <networpava@gmail.com>
2023-11-15 22:38:10 -0500

    View transtions

---
+++ src/data/Aplications.ts
@@ -0,0 +1,20 @@
+export const data =[
+    {
+        id: "74b6a7e0-7b3c-5a2a-b19e-8d642eea8c16",
+        title: "Audioplayer",
+        description: "un reproductor de audio con un diseño minimalista y elegante",
+        platform: "Pc",
+        price: "Gratis",
+        image: "https://res.cloudinary.com/daqrkk4sr/image/upload/v1696624707/store-kun/AudioPlayer/banner_y1ibhz_h8yvfc.png",
+        logo: "https://res.cloudinary.com/daqrkk4sr/image/upload/v1696623694/store-kun/AudioPlayer/logo_previ_tbhpj0.png",
+        version: "0.3.0",
+        images: [
+            "https://res.cloudinary.com/daqrkk4sr/image/upload/v1696623745/store-kun/AudioPlayer/vista_vnuzgb.png",
+            "https://res.cloudinary.com/daqrkk4sr/image/upload/v1696623920/store-kun/AudioPlayer/img_1_hjmwcy.png",
+            "https://res.cloudinary.com/daqrkk4sr/image/upload/v1696623992/store-kun/AudioPlayer/img_2_ov9cz0.png",
+            "https://res.cloudinary.com/daqrkk4sr/image/upload/v1696624051/store-kun/AudioPlayer/img_3_pl8pw3.png"
+        ],
+        info: "Un reproductor de audio local sin conexión a internet con un diseño minimalista y elegante que puedes personalizar a tu gusto o preferencias,IMPORTANTE: este reproductor no reproduce música de Spotify, YouTube, etc. solo reproduce música que tengas en tu dispositivo que tenga formato mp3 asta el momento. Para implementar música en el solo tienes que ponerla en la carpeta música que se encuentra usuario/música, para cambiar el fondo de tu aplicación tienes que crear una carpeta en usuario/Videos, que se llame fondos y poner los videos que queras de fondo, solo formato MP4, y ahora DISFRUTA DE TU MÚSICA FAVORITA.",
+        download: "https://www.mediafire.com/file/54qyqdfvqmrx0jq/Audioplayer_0.3.0_x64-setup.exe/file"
+    }
+]
