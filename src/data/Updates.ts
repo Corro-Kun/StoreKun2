1d63898c1603b4bb58e40f05718f123dfc4bcfc3
Corro-Kun <networpava@gmail.com>
2023-11-17 17:27:10 -0500

    add updates

---
+++ src/data/Updates.ts
@@ -0,0 +1,11 @@
+export const data = [
+    {
+        id: "74b6a7e0-7b3c-5a2a-b19e-8d642eea8c16",
+        title: "Audioplayer",
+        version: "0.3.0",
+        image: "https://res.cloudinary.com/daqrkk4sr/image/upload/v1696624707/store-kun/AudioPlayer/banner_y1ibhz_h8yvfc.png",
+        data: "Oct. 05, 2023",
+        description: "Se libero la primera versión de la aplicación",
+        link: "https://github.com/Corro-Kun/Audioplayer/releases/tag/v0.3.0-beta"
+    }
+]
---
+++ src/data/Wallpapers.ts
@@ -0,0 +1,8 @@
+export const wallpapers = [
+    "https://res.cloudinary.com/daqrkk4sr/video/upload/v1700077708/kf0afnueogzmfwykhxkl.mp4",
+    "https://res.cloudinary.com/daqrkk4sr/video/upload/v1700093809/yunx8ic7tnqjb5hssyci.mp4",
+    "https://res.cloudinary.com/daqrkk4sr/video/upload/v1700180593/zq985xqrnf2uj929ee18.mp4",
+    "https://res.cloudinary.com/daqrkk4sr/video/upload/v1700181257/kjkomyxlinwy5x8i6gqy.mp4",
+    "https://res.cloudinary.com/daqrkk4sr/video/upload/v1700181312/hhbp4po9iifrcn2mchmj.mp4",
+    "https://res.cloudinary.com/daqrkk4sr/video/upload/v1700181395/czfxsg0vuusv43awlv2e.mp4"
+]
