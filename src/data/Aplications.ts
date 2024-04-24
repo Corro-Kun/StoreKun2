4bebee1ee81af35b854fd71c9975c66b613baaa8
Corro-Kun <networpava@gmail.com>
2024-04-24 10:29:54 -0500

    add update of reminders 1.1.0

--- src/data/Aplications.ts
+++ src/data/Aplications.ts
@@ -7,7 +7,7 @@ export const data =[
         price: "Gratis",
         image: "https://res.cloudinary.com/daqrkk4sr/image/upload/v1703624734/store-kun/Reminders/hicacfyzg6yqyxwp0agw.png",
         logo: "https://res.cloudinary.com/daqrkk4sr/image/upload/v1696623694/store-kun/AudioPlayer/logo_previ_tbhpj0.png",
-        version: "1.0.0",
+        version: "1.1.0",
         images: [
             "https://res.cloudinary.com/daqrkk4sr/image/upload/v1703629386/store-kun/Reminders/ox1kilp3lpje6ycp14xn.png",
             "https://res.cloudinary.com/daqrkk4sr/image/upload/v1703629902/store-kun/Reminders/z9e5tc6ujbegn4xqqna5.png",
@@ -16,8 +16,15 @@ export const data =[
             "https://res.cloudinary.com/daqrkk4sr/image/upload/v1703630787/store-kun/Reminders/p9jwxvijg7wjy3fb5iop.png"
         ],
         info: "Un gestor personal offline con un diseño elegante y minimalista para recordar y organizar tu colección de anime, manga, películas, series y mucho más, Para agregar contenido, simplemente crea una sesión como anime, manga, películas, series, etc. a partir de hay solo tienes que agregar una ficha, con una imagen, descripción, link, etc. y listo, ya tienes tu contenido agregado, para verlo solo tienes que ir a la sesión que lo agregaste y listo, IMPORTANTE: esta aplicación no reproduce anime, manga, películas, series, etc. solo es para recordar y organizar tu colección de anime, manga, películas, series, etc. que tengas en tu dispositivo y ¡Disfruta de la comodidad de tener toda tu colección multimedia a mano y organízala a tu gusto con esta aplicación!",
-        download: "https://www.mediafire.com/file/x5x60qtmdydowm9/Reminders.apk/file",
+        download: "https://github.com/Corro-Kun/Remember.App/releases/download/v1.1.0/Remember.apk",
         updates:[
+            {
+                version: "1.1.0",
+                data: "Abr. 13, 2024",
+                description: "Se añadió la función de eliminar y actualizar fichas y solución de problemas",
+                link: "https://github.com/Corro-Kun/Remember.App/releases/tag/v1.1.0"
+            }
+            ,
             {
                 version: "1.0.0",
                 data: "Diciembre. 26, 2023",
